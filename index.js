d62553a61446b0f445bb71085946a7c76b5f4c73
Joe Hand <joe@joeahand.com>
2016-07-15 13:39:49 -0700

    use ansi diff and nicer printing

--- index.js
+++ index.js
@@ -1,22 +1,63 @@
 var fs = require('fs')
 var path = require('path')
+var differ = require('ansi-diff-stream')
+var prettyHrtime = require('pretty-hrtime')
+var ora = require('ora')
 var datTest = require('./lib/dat-test')
 
+var diff = differ()
 var datasets = path.join(__dirname, 'test', 'datasets')
 
 testDir(path.join(datasets, 'basic'))
 
 function testDir(dir, next) {
-  console.info('Starting', dir)
+  var lines = []
+  var msg = 'Starting '
+  var start = process.hrtime()
+  var spinner = ora({text: 'Running'})
+
+  setInterval(function () {
+    diff.write(print())
+  }, 200)
+  diff.pipe(process.stdout)
+  spinner.start()
+
+  function print () {
+    lines[0] = msg + dir
+    lines[1] = 'Elapsed time: ' + prettyHrtime(process.hrtime(start)) + '\n'
+    return lines.join('\n')
+  }
+
   datTest(dir, function (err, timers) {
     if (err) onerror(err)
-    console.info('\nResults')
-    console.info('Share time: %ds %dms', timers.share[0], timers.share[1] / 1000000)
-    console.info('Download time: %ds %dms', timers.download[0], timers.download[1] / 1000000)
-    console.info('Total time: %ds %dms', timers.all[0], timers.all[1] / 1000000)
-    console.info('\n\n')
+    lines[0] = 'Results ' + dir
+    lines.push('Share time: ' + prettyHrtime(timers.share))
+    lines.push('Download time: ' + prettyHrtime(timers.download))
+    lines.push('Total time: ' + prettyHrtime(timers.all) + '\n')
     if (next) next()
+    else {
+      diff.write(lines.join('\n'))
+      console.log(jsonResults(dir, timers))
+      process.exit(0)
+    }
   })
+}
+
+function jsonResults (folder, timers) {
+  var out = {
+    folder: folder,
+    results: {
+      share: toMiliSec(timers.share),
+      download: toMiliSec(timers.download),
+      total: toMiliSec(timers.all)
+    }
+  }
+
+  return JSON.stringify(out)
+
+  function toMiliSec (hrArray) {
+    return (hrArray[0] * 1000000 + hrArray[1] / 1000) / 1000
+  }
 }
 
 function onerror (err) {
